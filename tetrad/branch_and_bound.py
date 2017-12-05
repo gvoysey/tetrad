95a687f6102aa2e165ca1fce5b38c0e169ec8611
Graham Voysey <gvoysey@bu.edu>
2017-12-04 23:54:38 -0500

    iteration

--- tetrad/branch_and_bound.py
+++ tetrad/branch_and_bound.py
@@ -1,6 +1,20 @@
 """Prune a tree of tetrads."""
 import numpy as np
 import anytree
+from anytree.importer import JsonImporter
+from anytree import Node
+import json
+import sys
 
-def main(tree):
-    pass
+
+def main():
+    importer = JsonImporter()
+    with open(sys.argv[1], 'r') as j:
+        json_tree = j.read().replace("_name", "name")
+    tree = importer.import_(json_tree)
+
+    print('tree')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
